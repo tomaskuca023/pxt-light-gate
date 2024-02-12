e0113a647edc278ebc4b2b467a1f9a3acda7fbb9
tomaskuca023 <146720075+tomaskuca023@users.noreply.github.com>
2024-02-12 13:24:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,49 @@
 let averageLightLevel: number = 0;
 let allowedOffset: number = 10;
 let scanCycles: number = 10;
+let timer: number = 0;
+let isRunning: boolean = false;
+let startTime: number = 0;
+let isCalibrated: boolean = false;
+radio.setGroup(68)
 
+
+//light level calibration
 input.onButtonPressed(Button.A, function(){
+    isCalibrated = false;
     averageLightLevel = 0;
     for (let i = 0; i < scanCycles; i++) {
         averageLightLevel += input.lightLevel();
-        basic.pause(50)
+        basic.pause(50);
     }
     averageLightLevel /= scanCycles;
+    isCalibrated = true
 })
 
+//timer display
+input.onButtonPressed(Button.B, function(){
+    basic.showNumber(timer/1000)
+})
+
+//timer
+radio.onReceivedString(function(receivedString:"vybehl") {
+    startTime = control.millis()
+    isRunning = true
+})
+
+//beep
 basic.forever(function() {
-    if (Math.abs(averageLightLevel - input.lightLevel()) > allowedOffset) {
-        music.playTone(800, 100)
+    //timer
+    if (isRunning && isCalibrated) {
+        timer = control.millis() - startTime
+        basic.showNumber(timer / 1000)
+}
+
+    //motion detected
+    if (Math.abs(averageLightLevel - input.lightLevel()) > allowedOffset && isRunning && isCalibrated) {
+        isRunning = false
+        radio.sendString("dobehl")
     }
-
-    console.log(input.lightLevel());
-    console.log(averageLightLevel);
-    console.log(averageLightLevel - input.lightLevel());
-
     basic.pause(100);
 })
+
